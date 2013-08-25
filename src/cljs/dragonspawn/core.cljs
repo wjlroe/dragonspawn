8506119819e0d9c7785f5672d028eedf70e0b664
William Roe <willroe@gmail.com>
2013-08-25 17:45:13 +0100

    Countdown from 10 seconds

--- src/cljs/dragonspawn/core.cljs
+++ src/cljs/dragonspawn/core.cljs
@@ -2,15 +2,16 @@
   (:use [domina :only [set-styles!]])
   (:require [goog.dom :as dom]
             [goog.style :as style]
+            [goog.Timer]
             [goog.events :as events]
             [goog.events.KeyCodes :as kcs]
-            [goog.events.EventType :as event-type]
-            ))
+            [goog.events.EventType :as event-type]))
 
 (def game-width 800)
 (def game-height 600)
 (def cell-size 32)
 (def aspectRatio (float (/ 4 3)))
+(def time-limit 10)
 
 (defn abs
   [number]
@@ -66,15 +67,35 @@
         [x y] (cell->coords x y)]
     (.drawImage draw-context sprite x y size size)))
 
-(defn render-world
+(defn draw-text
+  [{:keys [draw-context]} x y text]
+  (let [[x y] (cell->coords x y)]
+    (set! (.-font draw-context) "bold 1.5em sans-serif")
+    (set! (.-textBaseline draw-context) "top")
+    (set! (.-textAlign draw-context) "right")
+    (.fillText draw-context text x y)))
+
+(defn elapsed-time
+  [start-time]
+  (int (/ (- (js/Date.) start-time) 1000)))
+
+(defmulti render :state)
+
+(defmethod render :playing
   [state]
-  (let [{:keys [player]} @state
+  (let [{:keys [player start-time]} state
+        countdown (- time-limit (elapsed-time start-time))
         [player-x player-y] player]
     (doseq [x (range game-x-cells) y (range game-y-cells)]
-      (draw-sprite @state x y cell-size :grass))
-    (draw-sprite @state 1 2 cell-size :bones)
-    (draw-sprite @state 4 4 cell-size :flask)
-    (draw-sprite @state player-x player-y cell-size :player))
+      (draw-sprite state x y cell-size :grass))
+    (draw-sprite state 1 2 cell-size :bones)
+    (draw-sprite state 4 4 cell-size :flask)
+    (draw-sprite state player-x player-y cell-size :player)
+    (draw-text state game-x-cells 0 (str countdown " seconds"))))
+
+(defn render-world
+  [state]
+  (render @state)
   (.requestAnimationFrame js/window #(render-world state)))
 
 (defn boundary
@@ -96,6 +117,19 @@
                            :forward [player-x (dec player-y)]
                            :back [player-x (inc player-y)]
                            player)))))))
+
+(defn timeup?
+  [start-time]
+  (>= (elapsed-time start-time) time-limit))
+
+(defn check-game
+  [state]
+  (swap! state
+         (fn [current]
+           (let [{:keys [game-state start-time]} current]
+             (if (timeup? start-time)
+               (assoc current :state :finished)
+               current)))))
 
 (defn handle-keyevent
   [state keyevent]
@@ -137,24 +171,28 @@
                  {:width (px width)
                   :height (px height)
                   :marginTop (px top)
-                  :marginLeft (px left)})
-    ))
+                  :marginLeft (px left)})))
 
 (defn ^:export main
   []
   (let [surface (surface-element)
         draw-context (.getContext surface "2d")
+        timer (goog.Timer. 200)
         state (atom {:surface surface
                      :draw-context draw-context
                      :surface-width (.-width surface)
                      :surface-height (.-height surface)
+                     :start-time (js/Date.)
+                     :state :playing
                      :player [(pixels->cell (/ game-width 2))
                               (- (pixels->cell game-height) 1)]})]
     (set! (.-width surface) game-width)
     (set! (.-height surface) game-height)
-    (resize state)
+    (events/listen timer goog.Timer/TICK (partial check-game state))
     (events/listen js/window event-type/RESIZE (partial resize state))
     (events/listen js/window event-type/KEYDOWN (partial keydown state))
+    (.start timer)
+    (resize state)
     (log-state @state)
     (render-world state)))
 
